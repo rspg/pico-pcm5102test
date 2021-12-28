4ca099fb8c62781188fd7850d96dc2bce274abb2
yuki <hiroki_pcc@hotmail.com>
2021-12-28 11:23:38 +0900

    mp3

--- pcm5102test.cpp
+++ pcm5102test.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/select.h>
 #include <algorithm>
+#include <atomic>
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/sync.h"
 #include "hardware/dma.h"
 #include "hardware/irq.h"
 #include "hardware/pio.h"
 #include "hardware/interp.h"
 #include "hardware/clocks.h"
+#include "hardware/pll.h"
 #include "audio_i2s_16.pio.h"
 #include "audio_i2s_24.pio.h"
 #include "audio_i2s_32.pio.h"
@@ -29,7 +34,7 @@ struct audio_send_buffer_t
     int      count = 0;
     audio_send_buffer_t* next = nullptr;
 };
-audio_send_buffer_t audio_send_buffer[2];
+audio_send_buffer_t audio_send_buffer[4];
 audio_send_buffer_t audio_send_buffer_silent;
 audio_send_buffer_t* audio_send_buffer_request = nullptr;
 audio_send_buffer_t* audio_send_buffer_free = nullptr;
@@ -58,10 +63,11 @@ void push_audio_buffer(audio_send_buffer_t*& head, audio_send_buffer_t* next)
 
 int dma_chan = 0;
 
-const uint32_t recvbuf_size = 4096;
-uint8_t recvbuf[recvbuf_size];
-short outbuf[MAX_NCHAN * MAX_NGRAN * MAX_NSAMP];
-
+const uint32_t usb_recv_buf_size = 16*1024; 
+uint8_t usb_recv_buf[usb_recv_buf_size];
+uint32_t usb_recv_buf_read_pos = 0;
+uint32_t usb_recv_buf_write_pos = 0;
+critical_section_t usb_recv_cs;
 
 uint bits()
 {
@@ -95,7 +101,7 @@ static void update_pio_frequency(uint32_t sample_freq, uint sm) {
     }
     assert(divider < 0x1000000);
     pio_sm_set_clkdiv_int_frac(audio_pio, sm, divider >> 8u, divider & 0xffu);
-    printf("pio %u Hz\n", (int)(system_clock_frequency / (divider/256.f)));
+    printf("pio %u Hz (%u system clocks)\n", (int)(system_clock_frequency / (divider/256.f)), system_clock_frequency);
 }
 
 int16_t fpsin(int16_t i)
@@ -126,23 +132,6 @@ int16_t fpsin(int16_t i)
     y = (y+(1UL<<(q-a-1)))>>(q-a); // Rounding
 
     return c ? -y : y;
-}
-
-int read_data(uint8_t* buf, int size)
-{
-    int c = getchar_timeout_us(1000);
-    if(c == PICO_ERROR_TIMEOUT)
-        return -1;
-    
-    buf[0] = c;
-    for(int i = 1; i < size; ++i)
-    {
-        c = getchar_timeout_us(0);
-        if(c == PICO_ERROR_TIMEOUT)
-            return i;
-        buf[i] = c;
-    }
-    return size;
 }
 
 void dma_handler()
@@ -180,7 +169,7 @@ void init()
 
     gpio_set_function(PIN_DIN, GPIO_FUNC_PIO0);
     gpio_set_function(PIN_BCK, GPIO_FUNC_PIO0);
-    gpio_set_function(PIN_LRCK, GPIO_FUNC_PIO0);
+    gpio_set_function(PIN_LRCK, GPIO_FUNC_PIO0);   
 
     busy_wait_ms(100);
 
@@ -250,8 +239,156 @@ void init()
     dma_handler();
 }
 
+void core1_loop()
+{
+    printf("core1_loop\n");
+
+    auto receive_tx_data = [](uint8_t* buf, uint32_t size) -> uint32_t
+    {
+        if(size == 0)
+            return 0;
+
+        int c;
+        do{
+            c = getchar_timeout_us(1000000);
+        }while(c == PICO_ERROR_TIMEOUT);
+        
+        buf[0] = c;
+        for(uint32_t i = 1; i < size; ++i)
+        {
+            c = getchar_timeout_us(0);
+            if(c == PICO_ERROR_TIMEOUT)
+                return i;
+            buf[i] = c;
+        }
+        return size;
+    };
+
+    while(true)
+    {
+        critical_section_enter_blocking(&usb_recv_cs);
+        const uint32_t read_pos_raw = usb_recv_buf_read_pos;
+        const uint32_t write_pos_raw = usb_recv_buf_write_pos;
+        critical_section_exit(&usb_recv_cs);
+        
+        assert(read_pos_raw < write_pos_raw);
+
+        if(write_pos_raw - read_pos_raw < usb_recv_buf_size)
+        {
+            const auto read_pos = read_pos_raw&(usb_recv_buf_size - 1);
+            const auto write_pos = write_pos_raw&(usb_recv_buf_size - 1);
+
+            int size = 0;
+            if(write_pos < read_pos)
+                size = read_pos - write_pos;
+            else
+                size = usb_recv_buf_size - write_pos;
+
+            if(size > 0)
+            {
+                uint32_t bytes = receive_tx_data(usb_recv_buf + write_pos, size);
+                if(bytes > 0)
+                {
+                    //printf("write_pos %u ", usb_recv_buf_write_pos);
+
+                    critical_section_enter_blocking(&usb_recv_cs);
+                    usb_recv_buf_write_pos += bytes;
+                    critical_section_exit(&usb_recv_cs);
+
+                    //printf("%u\n", usb_recv_buf_write_pos);
+                }
+            }
+        }
+
+        busy_wait_ms(1);
+    }
+}
+
+uint32_t fetch_usb_tx_data(uint8_t* buf, uint32_t size)
+{
+    critical_section_enter_blocking(&usb_recv_cs);
+    const uint32_t read_pos_raw = usb_recv_buf_read_pos;
+    const uint32_t write_pos_raw = usb_recv_buf_write_pos;
+    critical_section_exit(&usb_recv_cs);
+
+    assert(read_pos_raw < write_pos_raw);
+
+    if(read_pos_raw >= write_pos_raw)
+        return 0;
+
+    size = std::min(size, write_pos_raw - read_pos_raw);
+
+    const auto read_pos = read_pos_raw&(usb_recv_buf_size - 1);
+    const auto write_pos = write_pos_raw&(usb_recv_buf_size - 1);
+
+    uint32_t read_size = 0;
+    if(write_pos <= read_pos)
+    {
+        const uint32_t part1_size = std::min(usb_recv_buf_size - read_pos, size);
+        memcpy(buf, usb_recv_buf + read_pos, part1_size);
+        read_size += part1_size;
+
+        const uint32_t part2_size = std::min(write_pos, size - part1_size);
+        if(part2_size > 0)
+        {
+            memcpy(buf + part1_size, usb_recv_buf, part2_size);
+            read_size += part2_size;
+        }
+    }
+    else
+    {
+        const uint32_t part_size = std::min(write_pos - read_pos, size);
+        memcpy(buf, usb_recv_buf + read_pos, part_size);
+        read_size += part_size;
+    }
+
+    //printf("read_pos %u %u ", size, usb_recv_buf_read_pos);
+
+    critical_section_enter_blocking(&usb_recv_cs);
+    usb_recv_buf_read_pos += read_size;
+    critical_section_exit(&usb_recv_cs);
+
+    //printf("%u\n", usb_recv_buf_read_pos);
+
+    return read_size;
+}
+
+void beep()
+{
+    uint32_t s = 0;
+    uint32_t d = 400;
+
+    printf("beep \n");
+
+    while(true)
+    {   
+        audio_send_buffer_t* buf;
+        do{
+            busy_wait_ms(1);
+            buf = pull_audio_buffer(audio_send_buffer_free);
+        }while(buf == nullptr);
+
+        for(int i = 0; i < 2304; i += 2)
+        {
+            buf->samples[i + 0] =
+            buf->samples[i + 1] = (int32_t)fpsin(s)<<18;
+            s += d;
+        }
+        buf->count = 2304;
+
+        push_audio_buffer(audio_send_buffer_request, buf);
+    }
+}
+
 void acceptMP3()
 {
+    critical_section_init(&usb_recv_cs);
+    multicore_launch_core1(core1_loop);
+
+    static const uint32_t recvbuf_size = 4*1024;
+    static uint8_t recvbuf[recvbuf_size];
+    static short outbuf[MAX_NCHAN * MAX_NGRAN * MAX_NSAMP];
+
     while(true)
     {
         printf("Ready\n");
@@ -260,14 +397,13 @@ void acceptMP3()
         MP3FrameInfo frameinfo;
         uint8_t* recvbuf_write_ptr = recvbuf;
         uint8_t* recvbuf_read_ptr = recvbuf;
+        int syncWordOffset = -1;
         
-        bool receive_completed = false;
-
         auto receive = [&](bool blocking)
         {
             auto databytes = recvbuf_write_ptr - recvbuf_read_ptr;
             auto writespace = recvbuf_size - (recvbuf_write_ptr - recvbuf);
-            if(recvbuf_read_ptr != recvbuf && writespace < recvbuf_size/4)
+            if(recvbuf_read_ptr != recvbuf)
             {
                 //printf("move to top\n");
                 std::move(recvbuf_read_ptr, recvbuf_write_ptr, recvbuf);
@@ -275,10 +411,11 @@ void acceptMP3()
                 recvbuf_write_ptr = recvbuf + databytes;
                 writespace = recvbuf_size - databytes;
             }
-            int bytes = 0;
+            uint32_t bytes = 0;
             do{
-                bytes = read_data(recvbuf_write_ptr, writespace);
-            }while(bytes == -1 && blocking);
+                bytes = fetch_usb_tx_data(recvbuf_write_ptr, writespace);
+                busy_wait_ms(1);
+            }while(bytes == 0 && blocking);
             if(bytes > 0)
             {
                 recvbuf_write_ptr += bytes;
@@ -288,27 +425,23 @@ void acceptMP3()
             return databytes;
         };
 
-        bool datacompleted = false;
         while(true)
-        {
-            printf("loop\n");
-            
-            int databytes = receive(true);
+        {   
+            busy_wait_ms(1);
 
-            int offset = -1;
-            while(offset < 0)
+            int databytes = receive(false);
+
+            if(syncWordOffset < 0)
             {
-                offset = MP3FindSyncWord(recvbuf_read_ptr, databytes);
-                if(offset < 0)
-                    databytes = receive(true);
-            }
-            
-            //printf("MP3FindSyncWord %u\n", offset);
-            recvbuf_read_ptr += offset;
-            databytes -= offset;
+                syncWordOffset = MP3FindSyncWord(recvbuf_read_ptr, databytes);
+                if(syncWordOffset < 0)
+                    continue;
 
-            bool nextframe = false;
-            while(!nextframe)
+                //printf("MP3FindSyncWord %u\n", syncWordOffset);
+                recvbuf_read_ptr += syncWordOffset;
+                databytes -= syncWordOffset;
+            }
+            else
             {
                 //printf("MP3Decode %u\n", databytes);
 
@@ -330,23 +463,24 @@ void acceptMP3()
                             outofdata = true;
                             break;
                     }
-                    databytes = receive(true);
                 }
                 else
                 {
+                    //printf("ok\n");
                     recvbuf_read_ptr = inbuf;
                     databytes = bytesLeft;
+                    syncWordOffset = 0;
 
                     MP3GetLastFrameInfo(mp3dec, &frameinfo);
 
                     audio_send_buffer_t* buffer = nullptr;
                     do{
                         buffer = pull_audio_buffer(audio_send_buffer_free);
-                        databytes = receive(false);
+                        busy_wait_ms(1);
                     }while(buffer == nullptr);
                     
                     for(size_t i = 0; i < std::size(outbuf); ++i){
-                        buffer->samples[i] = outbuf[i]<<18;
+                        buffer->samples[i] = outbuf[i]<<16;
                         // printf("%04x ", std::abs(outbuf[i]));
                         // if(i%16==0)
                         //     printf("\n");
@@ -357,7 +491,6 @@ void acceptMP3()
                     push_audio_buffer(audio_send_buffer_request, buffer);
 
                     //printf("MP3GetLastFrameInfo %d %d\n", frameinfo.bitsPerSample, frameinfo.outputSamps);
-                    nextframe = true;
                 }
             }
         }
